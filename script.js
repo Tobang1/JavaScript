9c9d00d86a92d1e003c46d08fa67dc0571ed4ef2
Tobang1 <58491843+Tobang1@users.noreply.github.com>
2020-02-18 05:38:50 +0000

    conditions

--- script.js
+++ script.js
@@ -10,11 +10,29 @@ alert (add);
 }
 addsomething (5,2);
 
+//OPERATORS
+
+// = is used to asign everthing on right to every value on left
+// = = is used to examine a vlue to a variable or shronizing
 // Arithmetic and Assignment Operators.
-// compund operator make you combine = sign and other operator eg =+, =-.
+// compund operator make you combine = sign and other operator eg +=, -=.
+//increamental operator increase it by one ++, decrementis --,.
+// precedence operators is like BODMAS
 var number1 = 7;
 var number2 = 2;
 
 //var result = number1+number2;
-number1+=number2;
+number1 += number2;
 document.write (number1);
+
+//Coditions
+
+var a = 1;
+var b = 2;
+
+var result = (a+b);
+if (result <=3) {
+    document.write ( result)
+}
+
+
