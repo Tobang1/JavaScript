e5be3cbb33aed4f66eb7da364707ad4e5b255cf4
Tobang1 <58491843+Tobang1@users.noreply.github.com>
2020-02-18 05:17:25 +0000

    operators

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 // functions review
-// pass data into the ()
+// pass data into the ( but dont include var)
 // variables goes into the curly braces {}
 // you call your function after the braces.
 function addsomething (fruit, food) {
  
-var add = fruit+food;
+var add = fruit + food;
 alert (add);
 
 }
 addsomething (5,2);
+
+// Arithmetic and Assignment Operators.
+// compund operator make you combine = sign and other operator eg =+, =-.
+var number1 = 7;
+var number2 = 2;
+
+//var result = number1+number2;
+number1+=number2;
+document.write (number1);
