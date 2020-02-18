b181c276c1779011642e13d5488b8c1ccfdde1ba
Tobang1 <58491843+Tobang1@users.noreply.github.com>
2020-02-18 04:53:58 +0000

    function review

---
+++ script.js
@@ -0,0 +1,11 @@
+// functions review
+// pass data into the ()
+// variables goes into the curly braces {}
+// you call your function after the braces.
+function addsomething (fruit, food) {
+ 
+var add = fruit+food;
+alert (add);
+
+}
+addsomething (5,2);
